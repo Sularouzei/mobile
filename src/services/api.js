23279df8e2a8269fd1cf821f09c1dfb0a91e490a
Seu Nome <seu_email@example.comclear>
2024-11-04 18:19:42 -0300

    mobile

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.15.149:3333'
+   // baseURL: 'http://192.168.15.149:3333' // IP CASA PAI
+    baseURL: 'http://192.168.15.149:3333' // IP ESTACIO
 });
 
 export default api;
