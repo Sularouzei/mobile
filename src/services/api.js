278c9dede957aeccd8b82a0c1495896f0a7e7f8a
Seu Nome <seu_email@example.comclear>
2024-11-04 18:39:34 -0300

    adicionado novo ip estacio

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
    // baseURL: 'http://192.168.15.149:3333' // IP CASA PAI
-    baseURL: 'http://192.168.15.149:3333' // IP ESTACIO
+    baseURL: 'http://172.17.1.108:3333' // IP ESTACIO
 });
 
 export default api;
